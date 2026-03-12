b50eda66efe2f91bdacb9365c12d43c65deb0ee0
colin <kingflight123@gmail.com>
2026-03-12 21:30:06 +0800

    Add complementary pwm

--- src/main.c
+++ src/main.c
@@ -1,18 +1,57 @@
 #include "device.h"
 #include "driverlib.h"
 
+#define PWM_PERIOD_TICKS    1000U
+#define PWM_DUTY_TICKS      500U
+#define PWM_DEADBAND_TICKS  50U
+
+static void configureEPWM1Complementary(void)
+{
+    GPIO_setPinConfig(GPIO_0_EPWM1_A);
+    GPIO_setPinConfig(GPIO_1_EPWM1_B);
+    GPIO_setPadConfig(0U, GPIO_PIN_TYPE_STD);
+    GPIO_setPadConfig(1U, GPIO_PIN_TYPE_STD);
+
+    SysCtl_disablePeripheral(SYSCTL_PERIPH_CLK_TBCLKSYNC);
+
+    EPWM_setClockPrescaler(EPWM1_BASE, EPWM_CLOCK_DIVIDER_1,
+                           EPWM_HSCLOCK_DIVIDER_1);
+    EPWM_setTimeBasePeriod(EPWM1_BASE, PWM_PERIOD_TICKS);
+    EPWM_setTimeBaseCounter(EPWM1_BASE, 0U);
+    EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A,
+                                PWM_DUTY_TICKS);
+    EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_UP_DOWN);
+    EPWM_disablePhaseShiftLoad(EPWM1_BASE);
+
+    EPWM_setActionQualifierAction(EPWM1_BASE, EPWM_AQ_OUTPUT_A,
+                                  EPWM_AQ_OUTPUT_HIGH,
+                                  EPWM_AQ_OUTPUT_ON_TIMEBASE_UP_CMPA);
+    EPWM_setActionQualifierAction(EPWM1_BASE, EPWM_AQ_OUTPUT_A,
+                                  EPWM_AQ_OUTPUT_LOW,
+                                  EPWM_AQ_OUTPUT_ON_TIMEBASE_DOWN_CMPA);
+
+    EPWM_setDeadBandDelayMode(EPWM1_BASE, EPWM_DB_RED, true);
+    EPWM_setDeadBandDelayMode(EPWM1_BASE, EPWM_DB_FED, true);
+    EPWM_setRisingEdgeDelayCount(EPWM1_BASE, PWM_DEADBAND_TICKS);
+    EPWM_setFallingEdgeDelayCount(EPWM1_BASE, PWM_DEADBAND_TICKS);
+    EPWM_setDeadBandDelayPolarity(EPWM1_BASE, EPWM_DB_RED,
+                                  EPWM_DB_POLARITY_ACTIVE_HIGH);
+    EPWM_setDeadBandDelayPolarity(EPWM1_BASE, EPWM_DB_FED,
+                                  EPWM_DB_POLARITY_ACTIVE_LOW);
+    EPWM_setDeadBandOutputSwapMode(EPWM1_BASE, EPWM_DB_OUTPUT_A, false);
+    EPWM_setDeadBandOutputSwapMode(EPWM1_BASE, EPWM_DB_OUTPUT_B, false);
+
+    SysCtl_enablePeripheral(SYSCTL_PERIPH_CLK_TBCLKSYNC);
+}
+
 int main(void)
 {
     Device_init();
     Device_initGPIO();
 
-    GPIO_setPinConfig(DEVICE_GPIO_CFG_LED1);
-    GPIO_setPadConfig(DEVICE_GPIO_PIN_LED1, GPIO_PIN_TYPE_STD);
-    GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED1, GPIO_DIR_MODE_OUT);
+    configureEPWM1Complementary();
 
     while(1)
     {
-        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
-        DEVICE_DELAY_US(100000);
     }
 }
